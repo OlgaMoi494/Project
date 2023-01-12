b65d174e25ee9c7b3c277c40582fd7003b196fb5
OlgaMoi494 <olgamoi494@gmail.com>
2023-01-13 02:16:38 +0300

    байт строки

---
+++ Lesson3/bytes.py
@@ -0,0 +1,5 @@
+print('байты'.encode('utf-8')) # 1-й способ кодирования
+print(bytes('байты','utf-8')) # 2-й способ кодирования
+print('bytes'.encode('utf-8'))
+print(type('bytes'.encode('utf-8'))) # байтовый тип данных
+

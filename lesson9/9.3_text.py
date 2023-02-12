d89432c8788707902b3eb0ec56d20f9e94f81615
OlgaMoi494 <olgamoi494@gmail.com>
2023-02-12 18:36:20 +0300

    corrected reading txt file in cycle by lines

--- lesson9/9.3_text.py
+++ lesson9/9.3_text.py
@@ -4,16 +4,19 @@
 встречается в тексте (без учёта регистра). То есть буквы n и N считать
 одинаковыми.'''
 
-with open('text.txt', 'r') as txt_file:
-    text = txt_file.read()
+
+def counter_letters(letter: str, text_to_look: str) -> int:
+    '''prints the number of times the letter encounters in the text'''
+    letter_count = text_to_look.lower().count(letter)
+    return letter_count
+
 
 input_letter = input('Please enter a Latin letter: ').lower()
 
 
-def counter_letters(letter: str, text_to_look: str) -> None:
-    '''prints the number of times the letter encounters in the text'''
-    letter_count = text_to_look.lower().count(letter)
-    print(f'Letter "{letter}" is found {letter_count} in the text.')
+with open('text.txt', 'r') as txt_file:
+    count_letters = 0
+    for line in txt_file:
+        count_letters += counter_letters(input_letter, line)
 
-
-counter_letters(input_letter, text)
+print(f'Letter "{input_letter}" is found {count_letters} time in the text.')

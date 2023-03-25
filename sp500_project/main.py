97aa0a209bc3ba827374c2c786b1b52bba1793e0
OlgaMoi494 <olgamoi494@gmail.com>
2023-03-25 21:11:26 +0300

    main file, classes added

--- sp500_project/main.py
+++ sp500_project/main.py
@@ -13,7 +13,6 @@ from business_logic import (
     update_name,
     delete_company,
     truncate_all,
-    read_lst_of_sectors,
     random_data
 )
 from validators import (
@@ -23,6 +22,8 @@ from validators import (
     validate_price,
     validate_number_of_entries
 )
+from providers import provide_db
+from config import DB_FILE, DB_TYPE
 
 
 def main_program() -> None:
@@ -48,65 +49,72 @@ def main_program() -> None:
 
         if your_choice == '1':
             name = input('Enter the name of company: ').lower()
-            info_by_name(name)
+            info_by_name(name, provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '2':
             symbol = input('Enter the symbol of company: ').lower()
-            info_by_symbol(symbol)
+            info_by_symbol(symbol, provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '3':
-            print(f'List of sectors available: {read_lst_of_sectors()}')
+            print(
+                f'List of sectors available: '
+                f'{provide_db(DB_TYPE, DB_FILE).read_lst_of_sectors()}')
             sector = input('Enter the sector for search: ').lower()
-            info_by_sector(sector)
+            info_by_sector(sector, provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '4':
-            average_price()
+            average_price(provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '5':
-            top10_companies()
+            top10_companies(provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '6':
             while True:
                 symbol = input(
                     'Please enter symbol(min 3 - max 6 characters):\n').upper()
                 if validate_symbol_input(symbol):
-                    if validate_symbol_not_in_db(symbol):
+                    if validate_symbol_not_in_db(symbol,
+                                                 provide_db(DB_TYPE, DB_FILE)):
                         break
             while True:
                 name = input(
                     'Please enter name(min 3 - max 50 characters):\n').title()
-                if validate_name_input(name):
-                    if validate_name_not_in_db(name):
+                if validate_name_input(name, provide_db(DB_TYPE, DB_FILE)):
+                    if validate_name_not_in_db(name,
+                                               provide_db(DB_TYPE, DB_FILE)):
                         break
             while True:
                 print('Please enter sector(choose from the database)')
-                for single_sector in read_lst_of_sectors():
+                for single_sector in provide_db(DB_TYPE,
+                                                DB_FILE).read_lst_of_sectors():
                     print(single_sector)
                 sector = input('Sector: ').title()
-                if validate_sector_input(sector):
+                if validate_sector_input(sector, provide_db(DB_TYPE, DB_FILE)):
                     break
             while True:
                 price = input('Please enter price:\n')
                 if validate_price(price):
                     break
-            add_new_company(symbol, name, sector, price)
+            add_new_company(symbol, name, sector, price,
+                            provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '7':
             while True:
                 symbol = input(
                     'Please enter full symbol of the company updated: '
                 ).upper()
-                if validate_symbol_in_db(symbol):
+                if validate_symbol_in_db(symbol, provide_db(DB_TYPE, DB_FILE)):
                     break
             while True:
                 new_name = input(
                     'Enter a new name: ').title()
-                if validate_name_not_in_db(new_name):
+                if validate_name_not_in_db(new_name,
+                                           provide_db(DB_TYPE, DB_FILE)):
                     break
-            update_name(symbol, new_name)
+            update_name(symbol, new_name, provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '8':
             while True:
                 symbol = input(
                     'Please enter the symbol of the company deleted: ').upper()
-                if validate_symbol_in_db(symbol):
+                if validate_symbol_in_db(symbol, provide_db(DB_TYPE, DB_FILE)):
                     break
-            delete_company(symbol)
+            delete_company(symbol, provide_db(DB_TYPE, DB_FILE))
         elif your_choice == '9':
-            truncate_all()
+            truncate_all(provide_db(DB_TYPE, DB_FILE))
 
         elif your_choice == '10':
             while True:
@@ -114,7 +122,7 @@ def main_program() -> None:
                     'Please enter the number of entries to create: ')
                 if validate_number_of_entries(number_of_entries):
                     break
-            random_data(number_of_entries)
+            random_data(number_of_entries, provide_db(DB_TYPE, DB_FILE))
             print(f'Data in number of {number_of_entries} entries created!')
         elif your_choice == '11':
             print('GOODBYE')
